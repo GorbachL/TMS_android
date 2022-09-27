6fece42dd6995347343859ddad3b638fcea6768e
GorbachL <52042279+GorbachL@users.noreply.github.com>
2022-09-27 16:50:57 +0300

    Added Task 3

---
+++ src/HomeWork2/Task3.java
@@ -0,0 +1,29 @@
+package HomeWork2;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Task 3");
+
+        /*
+        Пользователь задает целое число, больше или равно 100 (например, 3801).
+        Определить третью цифру справа в этом числе (в данном случае 8)
+         */
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter number greater than or equal to 100");
+            int b = scanner.nextInt();
+
+            String s = "" + b;
+            System.out.println("The number of characters in this digit is - " + s.length() +
+                    "\n" + "You entered: " + b + "\n" +
+                    "The third digit from the right is - " + s.charAt(s.length() - 3));
+        } catch (Exception e) {
+            System.out.println("Invalid value..");
+        }
+    }
+}
